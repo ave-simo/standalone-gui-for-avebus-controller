3dc189e55119488977246f1c5aa58898123ac920
simone.francalancia <simone.francalancia@AVE-7CYPU3YFHWG>
2026-01-28 15:42:35 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace standalone_gui_for_avebus_controller
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
